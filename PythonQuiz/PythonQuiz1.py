3136c3816ed995952b3e7fd3325df77e72b09fb8
CharleneMcIntosh <cookiepringle7@gmail.com>
2018-08-07 11:11:55 -0400

    quizpy

---
+++ PythonQuiz/PythonQuiz1.py
@@ -0,0 +1,34 @@
+x=int(raw_input("Enter a number"))
+y=int(raw_input("Enter a number
+
+
+#Sum of Variables
+print ("{0}+{1}={2}").format(x,y,x+y))
+
+
+print ("%s +%s = %s "%(x,y,x=y))
+
+
+print (str(x)+"+"+stri(y)+"="+str(x+y))
+
+
+print x,"+",y,"=",*+y
+
+#Question C Difference of Variables
+
+print "%s -%s = %s" %(x,y,x-y)
+
+#Question D Multiplication of Variables
+
+print x, "*", y, "=", x*y
+
+#Question E Division of Variables
+
+if not y==0:
+    print "%s / %s" %(x,y, x/y)
+
+if y!=0:
+    print "{0} % {1} = {2}" format (x,y,x%y)
+
+    # y<>0 means not equal .. y is not equal to zero
+    
